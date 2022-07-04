052809cd4f1a7d394049eef5dbf5637dff164951
ck-kor <ckdrb48@naver.com>
2022-07-04 23:28:40 +0900

    jenkins_test_003

--- src/main/java/com/example/sky_jenkins/HelloController.java
+++ src/main/java/com/example/sky_jenkins/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String sayHello() {
-        return "halo Jenkins!";
+        return "haloo Jenkins!";
     }
 }
