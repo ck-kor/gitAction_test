fadd8255b11c55b11e3ba84d1db4efb6deab367c
ck-kor <ckdrb48@naver.com>
2022-07-04 21:55:41 +0900

    init 초기화

---
+++ src/main/java/com/example/sky_jenkins/SkyJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sky_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sky_jenkins/SkyJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sky_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
